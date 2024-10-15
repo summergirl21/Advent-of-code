6b55a7cec7a5b562d0b4732459725a03b3ccabfb
summergirl21 <stager.madeline@gmail.com>
2024-10-15 14:39:19 -0500

    Solution for 2018 day 9

--- 2018/Day09/PuzzleSolution9.py
+++ 2018/Day09/PuzzleSolution9.py
@@ -2,6 +2,9 @@
 """
 Part 1 Solution
 """
+from __future__ import annotations
+import time
+from dataclasses import dataclass
 
 DEBUG = False
 
@@ -22,57 +25,46 @@ def process_file(file_name):
     with open(file_name) as file:
         lines = list(map(lambda x: x.strip(), file.readlines()))
         print("Part 1")
+        start = time.time()
         solve_part1(lines)
+        end = time.time()
+        print(f"Elapsed time {end-start}")
 
-        # print("\nPart 2")
-        # solve_part2(lines)
+        print("\nPart 2")
+        start = time.time()
+        solve_part2(lines)
+        end = time.time()
+        print(f"Elapsed time {end-start}")
 
 
 def find_high_score(num_players: int, num_marbles: int) -> int:
     player_scores = [0] * num_players
     marbles = [0]
-    # log(f"Scores: {player_scores}")
 
     cur_mar_idx = 0
     player = 0
     for m in range(1, num_marbles+1):
-        # log(f"Player {player}")
         if m % 23 == 0:
             m2 = marbles[cur_mar_idx - 7]
-            # log(f"Score! {m}, {m2}, Player: {player}")
-            # log(f"idx: {cur_mar_idx}, Marbles: {marbles}")
-            debug = False
-            if cur_mar_idx < 7:
-                debug = True
-                log(f"LOW INDEX SCORE, cur_mar_idx: {cur_mar_idx}, m: {m}, m2: {m2}, len: {len(marbles)}, player: {player}")
-                log(f"PRE Scores: {player_scores}")
-                log(f"PRE SCORE: idx: {cur_mar_idx}, cur_mar: {marbles[cur_mar_idx]}, Marbles: {marbles}")
             player_scores[player] += m+m2
-            # log(f"Scores: {player_scores}")
 
             cur_mar_idx -= 7
             if cur_mar_idx < 0:
                 cur_mar_idx += len(marbles)
             marbles = marbles[:cur_mar_idx] + marbles[cur_mar_idx+1:]
 
-            if debug:
-                log(f"POST SCORE: idx: {cur_mar_idx}, cur_mar: {marbles[cur_mar_idx]}, len: {len(marbles)}, Marbles: {marbles}")
-                log(f"POST Scores: {player_scores}")
-            # log(f"idx: {cur_mar_idx}, Marbles: {marbles}")
         else:
             cur_mar_idx += 2
 
             if cur_mar_idx > len(marbles):
-                # log(f"wrapping: {cur_mar_idx}, {marbles}")
                 cur_mar_idx = cur_mar_idx - len(marbles)
 
-            # log(f"idx: {cur_mar_idx}")
             marbles.insert(cur_mar_idx, m)
 
         player = (player + 1) % num_players
 
-    # log(f"Marbles: {marbles}")
-    log(f"Scores: {player_scores}")
+    log(f"Marbles: {marbles}")
+    log(f"Scores: {player_scores}, Player: {player}")
     return max(player_scores)
 
 
@@ -86,9 +78,54 @@ def solve_part1(lines):
         print(f"Players: {players}, Marbles: {num_marbles}, High Score: {result}")
 
 
+@dataclass
+class Marble:
+    value: int
+    prev: Marble
+    next: Marble
+
+
+def find_high_score_fast(num_players: int, num_marbles: int) -> int:
+    player_scores = [0] * num_players
+    cur_mar = Marble(0, None, None)
+    cur_mar.next = cur_mar
+    cur_mar.prev = cur_mar
+    player = 0
+    for m in range(1, num_marbles + 1):
+        if m % 23 == 0:
+            log(f"Score")
+            for _ in range(7):
+                cur_mar = cur_mar.prev
+            m2 = cur_mar.value
+            player_scores[player] += m+m2
+
+            cur_mar.prev.next = cur_mar.next
+            cur_mar.next.prev = cur_mar.prev
+            cur_mar = cur_mar.next
+
+        else:
+            log(f"Insert marble")
+            cur_mar = cur_mar.next.next
+
+            new_mar = Marble(m, cur_mar.prev, cur_mar)
+            cur_mar.prev.next = new_mar
+            cur_mar.prev = new_mar
+            cur_mar = new_mar
+
+        player = (player + 1) % num_players
+
+    log(f"Scores: {player_scores}, Player: {player}")
+    return max(player_scores)
+
+
 def solve_part2(lines):
     for line in lines:
         log(line)
+        line = line.split(";")
+        players = int(line[0].split()[0])
+        num_marbles = int(line[1].split()[-2]) * 100
+        result = find_high_score_fast(players, num_marbles)
+        print(f"Players: {players}, Marbles: {num_marbles}, High Score: {result}")
 
 
 if __name__ == '__main__':
--- 2018/SolutionTemplate.py
+++ 2018/SolutionTemplate.py
@@ -2,6 +2,7 @@
 """
 Part 1 Solution
 """
+import time
 
 DEBUG = True
 
@@ -22,10 +23,16 @@ def process_file(file_name):
     with open(file_name) as file:
         lines = list(map(lambda x: x.strip(), file.readlines()))
         print("Part 1")
+        start = time.time()
         solve_part1(lines)
+        end = time.time()
+        print(f"Elapsed time {end - start}")
 
         # print("\nPart 2")
+        # start = time.time()
         # solve_part2(lines)
+        # end = time.time()
+        # print(f"Elapsed time {end - start}")
 
 
 def solve_part1(lines):
